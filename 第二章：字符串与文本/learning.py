80e544e83f0d31d3ccdddcf337e5f735a107cf36
MarvinLiang <961239951@qq.com>
2020-09-21 22:26:54 +0800

    Update learning.py

--- 第二章：字符串与文本/learning.py
+++ 第二章：字符串与文本/learning.py
@@ -15,3 +15,13 @@ def read_data(name):
     else:
         with open(name) as f:
             return f.read()
+        
+from fnmatch import fnmatch, fnmatchcase
+fnmatch('foo.txt', '*.txt')
+
+fnmatch('foo.txt', '?oo.txt')
+
+fnmatch('Dat45.csv', 'Dat[0-9]*')
+
+names = ['Dat1.csv', 'Dat2.csv', 'config.ini', 'foo.py']
+[name for name in names if fnmatch(name, 'Dat*.csv')]
