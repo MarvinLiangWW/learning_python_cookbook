d8279691de5a048c24601fbe4fd7b6103a204578
MarvinLiang <961239951@qq.com>
2020-09-21 15:58:38 +0800

    Update learning.py

--- 第一章：数据结构与算法/learning.py
+++ 第一章：数据结构与算法/learning.py
@@ -97,3 +97,11 @@ s = sum([x * x for x in nums])
 s = sum((x * x for x in nums)) # 显式的传递一个生成器表达式对象
 s = sum(x * x for x in nums) # 更加优雅的实现方式，省略了括号
   
+# 现在有多个字典或者映射，你想将它们从逻辑上合并为一个单一的映射后执行某些操作， 比如查找值或者检查某些键是否存在
+a = {'x': 1, 'z': 3 }
+b = {'y': 2, 'z': 4 }
+from collections import ChainMap
+c = ChainMap(a,b)
+print(c['x']) # Outputs 1 (from a)
+print(c['y']) # Outputs 2 (from b)
+print(c['z']) # Outputs 3 (from a)
