ab455771e413e5931dedd9e9ace361112af112ef
MarvinLiang <961239951@qq.com>
2020-09-21 14:14:20 +0800

    Update learning.py

--- 第一章：数据结构与算法/learning.py
+++ 第一章：数据结构与算法/learning.py
@@ -63,3 +63,24 @@ for date, items in groupby(rows, key=itemgetter('date')):
     print(date)
     for i in items:
         print(' ', i)
+
+# 如何用compress函数
+from itertools import compress
+addresses = [
+    '5412 N CLARK',
+    '5148 N CLARK',
+    '5800 E 58TH',
+    '2122 N CLARK',
+    '5645 N RAVENSWOOD',
+    '1060 W ADDISON',
+    '4801 N BROADWAY',
+    '1039 W GRANVILLE',
+]
+counts = [ 0, 3, 10, 4, 1, 7, 6, 1]
+more5 = [n > 5 for n in counts]
+list(compress(addresses, more5))
+
+# 类似于类的调用
+from collections import namedtuple
+Subscriber = namedtuple('Subscriber', ['addr', 'joined'])
+sub = Subscriber('jonesy@example.com', '2012-10-19')
