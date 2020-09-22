93f3030de4b56897b0db663d021ce4e155d3c22b
MarvinLiang <961239951@qq.com>
2020-09-22 20:31:14 +0800

    Update learning.py

--- 第三章：数字日期和时间/learning.py
+++ 第三章：数字日期和时间/learning.py
@@ -14,3 +14,19 @@ a = complex(2, 4)
 a.real
 a.imag
 a.conjugate()
+
+from fractions import Fraction
+
+from datetime import datetime
+def parse_ymd(s):
+    year_s, mon_s, day_s = s.split('-')
+    return datetime(int(year_s), int(mon_s), int(day_s))
+  
+# 这个函数比下面的strptime方法快7倍
+text = '2012-09-20'
+y = datetime.strptime(text, '%Y-%m-%d')
+
+from pytz import timezone
+
+
+
