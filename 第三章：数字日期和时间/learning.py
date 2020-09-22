f5262af16e01adac9a76688e49154c2eb1e2eec5
MarvinLiang <961239951@qq.com>
2020-09-22 18:32:06 +0800

    Create learning.py

---
+++ 第三章：数字日期和时间/learning.py
@@ -0,0 +1,16 @@
+# 学到了，用round还可以处理百位，
+
+
+# decimal包用来处理浮点数的
+from decimal import Decimal
+from decimal import localcontext
+
+
+#bin(),oct(),hex()
+
+# 复数
+import cmath
+a = complex(2, 4)
+a.real
+a.imag
+a.conjugate()
